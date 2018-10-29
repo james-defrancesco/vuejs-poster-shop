3ca7c637f269cd1844aa6b6fb6ba37774df78d58
James De Francesco <james.defrancesco@lendkey.com>
2018-10-29 17:05:34 -0400

    more cart functionality

--- public/script.js
+++ public/script.js
@@ -3,16 +3,26 @@ new Vue({
     data: {
         total: 0,
         items: [
-            { title: "Item 1" },
-            { title: "Item 2" },
-            { title: "Item 3" },
+            { id: 1, title: "Item 1" },
+            { id: 2, title: "Item 2" },
+            { id: 3, title: "Item 3" },
         ],
         cart: []
     },
     methods: {
         addItem: function(index) {
             this.total += 9.99;
-            this.cart.push(this.items[index]);
+            var item = this.items[index];
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    this.cart[i].qty++;
+                }
+            }
+            this.cart.push({
+                id: item.id,
+                title: item.title,
+                qty: 1
+            });
         }
     }
 });
