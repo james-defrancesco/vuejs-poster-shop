c2dde6683f563eeb84f997a0029ac88dc2e4d1be
James De Francesco <james.defrancesco@lendkey.com>
2018-10-30 13:54:19 -0400

    adds search functionality

--- public/script.js
+++ public/script.js
@@ -1,28 +1,59 @@
+const price = 9.99
+
 new Vue({
     el: '#app',   
     data: {
         total: 0,
-        items: [
-            { id: 1, title: "Item 1" },
-            { id: 2, title: "Item 2" },
-            { id: 3, title: "Item 3" },
-        ],
-        cart: []
+        items: [],
+				cart: [],
+				search: ''
     },
     methods: {
+				onSubmit: function() {
+					this.$http.get('/search/'.concat(this.search)).then(function(res) {
+						this.items = res.data
+					})
+				},
         addItem: function(index) {
-            this.total += 9.99;
+            this.total += price
             var item = this.items[index];
+            var found = false
             for (var i = 0; i < this.cart.length; i++) {
                 if (this.cart[i].id === item.id) {
-                    this.cart[i].qty++;
+                    found = true
+										this.cart[i].qty++
+										break
                 }
             }
-            this.cart.push({
-                id: item.id,
-                title: item.title,
-                qty: 1
-            });
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: price
+                });
+            }
+        },
+        inc: function(item) {
+            item.qty++
+            this.total += price
+        },
+        dec: function(item) {
+            item.qty--
+            this.total -= price
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1)
+                        break
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function (price) {
+            return '$'.concat(price.toFixed(2))
         }
     }
 });
